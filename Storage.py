c5e56385f5bfe2492f21587564a5e662fc8d3fd6
PoalChaynik <alpolaop13@gmail.com>
2022-04-11 12:46:25 +0300

    TAGAD PABEIGTS!!!

--- Storage.py
+++ Storage.py
@@ -10,15 +10,84 @@ db.execute("""CREATE TABLE IF NOT EXISTS storage
     )""")
 
 print('"PROJECT - NOLIKTAVA" GATAVA DARBAM!\n')
-def Redigesana():
+def Pievienosana():
+    while True:
+        name = input('Ievadiet Preces Nosaukumu: ')
+        if name.isdigit() == False:
+            if name.strip() == '':
+                print('Ievadiet Atkartoti Preces Nosaukumu!')
+                continue
+            else:
+                break
+        else:
+            print('Ievadiet Atkartoti Preces Nosaukumu!')
+            continue
 
-    izvele = input('1 - Pievienosana | 2 - Datu Mainisana:  ')
+    while True:
+        quantity = input('Ievadiet Preces Daudzumu: ')
+        if quantity.isdigit():
+            break
+        else:
+            print('Ievadiet Atkartoti Preces Daudzumu!')
+            continue
 
-    if izvele == '1':
+    while True:
+        price = input('Ievadiet Preces Cenu: ')
+        if price.isdigit():
+            break
+        else:
+            print('Ievadiet Atkartoti Preces Cenu!')
+            continue
+
+    # print(name,quantity,price)
+
+    db.execute("""INSERT INTO storage
+        (Name, Quantity, Price)
+        VALUES (:Name,:Quantity,:Price)
+    """,{'Name':name,'Quantity':quantity,'Price':price})
+
+    db.commit()
+
+def DatuMaina():
+    data = db.execute("""SELECT * FROM storage""")
+    print()
+    for i in data:
+        print('ID:',i[0],'|','Nosaukums:',i[1],'|','Daudzums:',i[2],'|','Cena:',i[3])
+    print()
+    while True:
+        id = input('Ievadiet ID Precei, Kuras Datus Gribat Mainit | 0 - Atcelt: ')
+        if id.isdigit():
+            if id == '0':
+                return
+            elif int(id) <= i[0]:
+                break
+            else:
+                print('Ievadiet Preces ID Velreiz!')
+                continue                   
+        else:
+            print('Ievadiet Preces ID Velreiz!')
+            continue
+    if id != '0':
+        print('Izvelaties Ko Jus Gribat Mainit?')
+    while True:
+        if id == '0':
+            return
+        editing1 = input('1 - Nosaukums | 2 - Daudzums | 3 - Cena: ')
+        if editing1.isdigit() and int(editing1) <= 3:
+            break
+        else:
+            print('Velreiz Izvelaties Ko Jus Gribat Mainit!')
+            continue
+
+    newName = 'Ievadiet Jaunu Preces Nosaukumu: '
+    newQuantity = 'Ievadiet Jaunu Preces Daudzumu: '
+    newPrice = 'Ievadiet Jaunu Preces Cenu: '
+
+    if editing1 == '1':
         while True:
-            name = input('Ievadiet Preces Nosaukumu: ')
-            if name.isdigit() == False:
-                if name.strip() == '':
+            newData = input(newName)
+            if newData.isdigit() == False:
+                if newData.strip() == '':
                     print('Ievadiet Atkartoti Preces Nosaukumu!')
                     continue
                 else:
@@ -27,106 +96,33 @@ def Redigesana():
                 print('Ievadiet Atkartoti Preces Nosaukumu!')
                 continue
 
+        newData = (newData,id)
+        db.execute("""UPDATE storage SET Name=? WHERE ID=?""", newData)
+        db.commit()
+
+    elif editing1 == '2':
         while True:
-            quantity = input('Ievadiet Preces Daudzumu: ')
-            if quantity.isdigit():
+            newData = input(newQuantity)
+            if newData.isdigit():
                 break
             else:
                 print('Ievadiet Atkartoti Preces Daudzumu!')
                 continue
 
+        newData = (newData,id)
+        db.execute("""UPDATE storage SET Quantity=? WHERE ID=?""", newData)
+        db.commit()
+    elif editing1 == '3':
         while True:
-            price = input('Ievadiet Preces Cenu: ')
-            if price.isdigit():
+            newData = input(newPrice)
+            if newData.isdigit():
                 break
             else:
                 print('Ievadiet Atkartoti Preces Cenu!')
                 continue
-
-        # print(name,quantity,price)
-
-        db.execute("""INSERT INTO storage
-            (Name, Quantity, Price)
-            VALUES (:Name,:Quantity,:Price)
-        """,{'Name':name,'Quantity':quantity,'Price':price})
-
+        newData = (newData,id)
+        db.execute("""UPDATE storage SET Price=? WHERE ID=?""", newData)
         db.commit()
-
-    elif izvele == '2':
-        data = db.execute("""SELECT * FROM storage""")
-        print()
-        for i in data:
-            print('ID:',i[0],'|','Nosaukums:',i[1],'|','Daudzums:',i[2],'|','Cena:',i[3])
-        print()
-        while True:
-            id = input('Ievadiet ID Precei, Kuras Datus Gribat Mainit | 0 - Atcelt: ')
-            if id.isdigit():
-                if id == '0':
-                    return
-                elif int(id) <= i[0]:
-                    break
-                else:
-                    print('Ievadiet Preces ID Velreiz!')
-                    continue                   
-            else:
-                print('Ievadiet Preces ID Velreiz!')
-                continue
-        if id != '0':
-            print('Izvelaties Ko Jus Gribat Mainit?')
-        while True:
-            if id == '0':
-                return
-            editing1 = input('1 - Nosaukums | 2 - Daudzums | 3 - Cena: ')
-            if editing1.isdigit() and int(editing1) <= 3:
-                break
-            else:
-                print('Velreiz Izvelaties Ko Jus Gribat Mainit!')
-                continue
-
-        newName = 'Ievadiet Jaunu Preces Nosaukumu: '
-        newQuantity = 'Ievadiet Jaunu Preces Daudzumu: '
-        newPrice = 'Ievadiet Jaunu Preces Cenu: '
-
-        if editing1 == '1':
-            while True:
-                newData = input(newName)
-                if newData.isdigit() == False:
-                    if newData.strip() == '':
-                        print('Ievadiet Atkartoti Preces Nosaukumu!')
-                        continue
-                    else:
-                        break
-                else:
-                    print('Ievadiet Atkartoti Preces Nosaukumu!')
-                    continue
-
-            newData = (newData,id)
-            db.execute("""UPDATE storage SET Name=? WHERE ID=?""", newData)
-            db.commit()
-
-        elif editing1 == '2':
-            while True:
-                newData = input(newQuantity)
-                if newData.isdigit():
-                    break
-                else:
-                    print('Ievadiet Atkartoti Preces Daudzumu!')
-                    continue
-
-            newData = (newData,id)
-            db.execute("""UPDATE storage SET Quantity=? WHERE ID=?""", newData)
-            db.commit()
-        elif editing1 == '3':
-            while True:
-                newData = input(newPrice)
-                if newData.isdigit():
-                    break
-                else:
-                    print('Ievadiet Atkartoti Preces Cenu!')
-                    continue
-            newData = (newData,id)
-            db.execute("""UPDATE storage SET Price=? WHERE ID=?""", newData)
-            db.commit()
 
 def Searching():
     while True:
@@ -170,12 +166,14 @@ def Searching():
 
 
 while True:
-    menu = input("MENU| 1 - Meklesana | 2 - Redigesana | 3 - Iziet: ")
+    menu = input("MENU| 1 - Meklesana | 2 - Datu Pievienosana | 3 - Datu Mainisana | 4 - Iziet: ")
     if menu == '2':
-        Redigesana()
+        Pievienosana()
     elif menu == '1':
         Searching()
     elif menu == '3':
+        DatuMaina()
+    elif menu == '4':
         db.close()
         exit()
     else:
